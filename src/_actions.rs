171fee9ada9bd1ce3f9a9981ffece3fdb10b881c
kennycallado <kennycallado@hotmail.com>
2023-09-17 12:22:48 +0200

    ADD Agrega más acciones al manager y separa públicas y privadas

---
+++ src/_actions.rs
@@ -0,0 +1,95 @@
+use tokio_cron_scheduler::{Job, JobScheduler};
+use uuid::Uuid;
+
+use crate::manager::EscalonJobsManager;
+use crate::{EscalonJobStatus, EscalonJobTrait, NewEscalonJob};
+
+impl<T: Clone + Send + Sync + 'static> EscalonJobsManager<T> {
+    pub async fn create_job(
+        &self,
+        new_cron_job: impl EscalonJobTrait<T> + Into<NewEscalonJob> + Clone + Send + Sync + 'static,
+    ) -> Uuid {
+        let new_job = new_cron_job.clone().into();
+        let manager = self.clone();
+
+        let job = Job::new_async(new_job.schedule.as_str(), move |uuid, lock| {
+            let new_job = new_cron_job.clone();
+            let manager = manager.clone();
+
+            Box::pin(async move {
+                manager.status_handler(uuid, lock, new_job).await;
+            })
+        })
+        .unwrap();
+
+        let scheduler;
+        {
+            scheduler = self.scheduler.lock().unwrap().clone();
+        }
+        scheduler.add(job).await.unwrap()
+    }
+
+    async fn status_handler(
+        &self,
+        uuid: Uuid,
+        mut lock: JobScheduler,
+        new_cron_job: impl EscalonJobTrait<T> + Into<NewEscalonJob> + Clone + Send + Sync + 'static,
+    ) {
+        let new_job = new_cron_job.clone().into();
+        let next_tick = lock.next_tick_for_job(uuid).await.unwrap().unwrap().naive_utc();
+
+        let status =
+            self.jobs.lock().unwrap().iter().find(|j| j.job_id == uuid).unwrap().status.clone();
+
+        match status {
+            EscalonJobStatus::Scheduled => {
+                if let Some(since) = new_job.since {
+                    if since > next_tick {
+                        return;
+                    }
+                }
+
+                self.update_status(uuid, EscalonJobStatus::Running);
+                new_cron_job.update_db(&self.get_job(uuid).await).await;
+            }
+            EscalonJobStatus::Running => {
+                let job = self
+                    .jobs
+                    .lock()
+                    .unwrap()
+                    .iter_mut()
+                    .find(|j| j.job_id == uuid)
+                    .unwrap()
+                    .clone();
+
+                let es_job = new_cron_job.run(job.clone(), self.context.clone()).await;
+
+                if es_job != job {
+                    self.update_status(uuid, es_job.status.to_owned());
+                    new_cron_job.update_db(&es_job).await;
+                }
+            }
+            EscalonJobStatus::Done | EscalonJobStatus::Failed => {
+                lock.remove(&uuid).await.unwrap();
+                new_cron_job.update_db(&self.get_job(uuid).await).await;
+            }
+        }
+
+        if let Some(until) = new_job.until {
+            if until < next_tick {
+                self.update_status(uuid, EscalonJobStatus::Done);
+                new_cron_job.update_db(&self.get_job(uuid).await).await;
+            }
+        }
+    }
+
+    fn update_status(&self, id: Uuid, status: EscalonJobStatus) {
+        let jobs = self.jobs.clone();
+
+        {
+            let mut jobs = jobs.lock().unwrap();
+            let job = jobs.iter_mut().find(|j| j.job_id == id).unwrap();
+            job.status = status;
+        }
+    }
+}
--- src/actions.rs
+++ src/actions.rs
@@ -1,112 +1,72 @@
-use tokio_cron_scheduler::{Job, JobScheduler};
 use uuid::Uuid;
 
-use crate::{EscalonJob, EscalonJobStatus, EscalonJobTrait, NewEscalonJob};
 use crate::manager::EscalonJobsManager;
+use crate::{EscalonJob, EscalonJobTrait, NewEscalonJob};
 
 impl<T: Clone + Send + Sync + 'static> EscalonJobsManager<T> {
-    pub async fn create_job(
+    pub async fn get_job(&self, id: Uuid) -> EscalonJob {
+        self.jobs.lock().unwrap().iter().find(|j| j.job_id == id).unwrap().clone()
+    }
+
+    pub async fn get_jobs(&self) -> Vec<EscalonJob> {
+        self.jobs.lock().unwrap().to_owned()
+    }
+
+    pub async fn add_job(
         &self,
         new_cron_job: impl EscalonJobTrait<T> + Into<NewEscalonJob> + Clone + Send + Sync + 'static,
     ) -> EscalonJob {
-        let cloned = new_cron_job.clone().into();
-        let new_job = new_cron_job.clone();
-        let manager = self.clone();
+        let new_job = new_cron_job.clone().into();
+        let job_id = self.create_job(new_cron_job).await;
 
-        let job =
-            Job::new_async(new_job.into().schedule.clone().as_str(), move |uuid, lock| {
-                let new_cron_job = new_cron_job.clone();
-                let manager = manager.clone();
-
-                Box::pin(async move {
-                    manager.status_handler(uuid, &lock, new_cron_job).await;
-                })
-            })
-            .unwrap();
-
-        let job_id;
-        {
-            let scheduler = self.scheduler.lock().unwrap().clone();
-            job_id = scheduler.add(job).await.unwrap();
-        }
-
-        let cron_job = EscalonJob {
+        let es_job = EscalonJob {
             job_id,
-            status: EscalonJobStatus::Scheduled,
-            schedule: cloned.schedule,
-            since: cloned.since,
-            until: cloned.until,
+            status: Default::default(),
+            schedule: new_job.schedule,
+            since: new_job.since,
+            until: new_job.until,
         };
 
-        self.jobs.lock().unwrap().push(cron_job.clone());
+        self.jobs.lock().unwrap().push(es_job.clone());
 
-        cron_job
+        es_job
     }
 
-    async fn status_handler(&self, uuid: Uuid, lock: &JobScheduler, new_cron_job: impl EscalonJobTrait<T> + Into<NewEscalonJob> + Clone + Send + Sync + 'static) {
-        let status = self.jobs
-            .lock()
-            .unwrap()
-            .iter()
-            .find(|j| j.job_id == uuid)
-            .unwrap()
-            .status
-            .clone();
-
-        match status {
-            EscalonJobStatus::Scheduled => {
-                // check things like since and until
-                // to change state to active
-
-                let job = self.jobs
-                    .lock()
-                    .unwrap()
-                    .iter()
-                    .find(|j| j.job_id == uuid)
-                    .unwrap()
-                    .clone();
-
-                self.update_state(uuid, EscalonJobStatus::Running);
-                new_cron_job.update_db(&job).await;
-            }
-            EscalonJobStatus::Running => {
-                let job = self.jobs
-                    .lock()
-                    .unwrap()
-                    .iter()
-                    .find(|j| j.job_id == uuid)
-                    .unwrap()
-                    .clone();
-
-                let returned = new_cron_job.run(job.clone(), self.context.clone()).await;
-
-                if returned != job {
-                    self.update_state(uuid, returned.status.to_owned());
-                }
-            }
-            EscalonJobStatus::Done | EscalonJobStatus::Failed => {
-                let job = self.jobs
-                    .lock()
-                    .unwrap()
-                    .iter()
-                    .find(|j| j.job_id == uuid)
-                    .unwrap()
-                    .clone();
-
-                lock.remove(&uuid).await.unwrap();
-                new_cron_job.update_db(&job).await;
-            }
+    pub async fn remove_job(&self, id: Uuid) {
+        let scheduler;
+        {
+            scheduler = self.scheduler.lock().unwrap().clone();
         }
+        scheduler.remove(&id).await.unwrap();
+
+        self.jobs.lock().unwrap().retain(|j| j.job_id != id);
     }
 
-    fn update_state(&self, uuid: Uuid, status: EscalonJobStatus) {
-        self.jobs
-            .lock()
-            .unwrap()
-            .iter_mut()
-            .find(|j| j.job_id == uuid)
-            .unwrap()
-            .status = status.to_owned();
-    }
+    pub async fn update_job(
+        &self,
+        id: Uuid,
+        new_cron_job: impl EscalonJobTrait<T> + Into<NewEscalonJob> + Clone + Send + Sync + 'static,
+    ) -> EscalonJob {
+        let new_job = new_cron_job.clone().into();
+        let jobs = self.jobs.clone();
 
+        let mut job;
+        {
+            job = jobs.lock().unwrap().iter().find(|j| j.job_id == id).unwrap().clone();
+        }
+
+        job.schedule = new_job.schedule;
+        job.since = new_job.since;
+        job.until = new_job.until;
+
+        let scheduler;
+        {
+            scheduler = self.scheduler.lock().unwrap().clone();
+        }
+        scheduler.remove(&id).await.unwrap();
+
+        self.create_job(new_cron_job).await;
+
+        job.clone()
+    }
 }
